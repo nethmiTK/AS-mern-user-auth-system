9ec9a04efb4df8b651e7072088b648d942879b37
nethmiTK <nethmitk33@gmail.com>
2025-04-17 09:09:57 +0530

    install multer

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  profilePic: { type: String, default: '' } // stores file name or relative path
 });
 
 module.exports = mongoose.model('User', UserSchema);
